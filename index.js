e7d2faa68a818732767b784ad92267ac8947138b
sheik oodally <sheikoodally@hotmail.com>
2019-09-11 20:17:50 -0400

    index,js

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-let arrayList = [
-];
+let arrayList = [];
+LiId = 0;
+let item = "";
 
-LiId = arrayList.length;
 
 function append(){
-    let pushList = "";
 
-    pushList = document.getElementById("appendTxt").value;
-    pushList.trim();
+    item = document.getElementById("appendTxt").value;
 
-    if(pushList === null  || pushList === "" || pushList === " "){
+    if(item === null  || item === "" || item === " "){
         alert("enter something");
     }
     else{
 
-        arrayList.push(pushList.trim());
-        var node = document.createElement("LI");
-        var textnode = document.createTextNode(pushList);
-        node.id = LiId;
-        node.appendChild(textnode);
-        document.getElementById("listClass").appendChild(node);
+        arrayList.push(item.trim());
+        var li = document.createElement("LI");
+        var textnode = document.createTextNode(item);
+        li.id = "liId";
+        li.appendChild(textnode);
+        document.getElementById("listClass").appendChild(li);
         LiId++;
     }
 };
 
 function deleteLi(){
-    var el = document.getElementById("5");
-    el.remove();
+    var ul = document.getElementById("listClass");
+    var txt = document.getElementById("appendTxt");
+    var liDel = document.getElementById("liId");
+    var delItem = document.getElementById(txt.value);
+    if (delItem == liDel){
+        ul.removeChild(item);
+    }
 }
+
